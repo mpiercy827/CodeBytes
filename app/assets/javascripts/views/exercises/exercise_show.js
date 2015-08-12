54a1f54cadfe263129484af691d69070683b2cd3
mpiercy827 <mpiercy@gmail.com>
2015-08-12 16:22:52 -0700

    add class to exercise info

--- app/assets/javascripts/views/exercises/exercise_show.js
+++ app/assets/javascripts/views/exercises/exercise_show.js
@@ -1,6 +1,8 @@
 CodeBytes.Views.ExerciseShow = Backbone.View.extend({
   template: JST["exercises/exercise_show"],
 
+  className: "exercise-text",
+
   render: function () {
     var content = this.template({ exercise: this.model });
     this.$el.html(content);
--- app/assets/javascripts/views/topics/topic_show.js
+++ app/assets/javascripts/views/topics/topic_show.js
@@ -4,19 +4,27 @@ CodeBytes.Views.TopicShow = Backbone.CompositeView.extend({
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
     this.listenTo(this.model.exercises(), "add", this.addExercise);
-    this.model.exercises().each(function (exercise) {debugger;});
   },
 
   addExercise: function (exercise) {
-    // if (this.subviews(".exercise") != []) {
-    //   var oldExerciseView = this.subviews(".exercise");
-    //   oldExerciseView && oldExerciseView.remove();
-    // }
     var exerciseView = new CodeBytes.Views.ExerciseShow({ model: exercise });
     this.addSubview(".exercise", exerciseView);
   },
 
+  // activeExercise: function () {
+  //   if (!this._active && this.model.exercises().length !== 0) {
+  //     this._active = this.model.exercises().first();
+  //   }
+  //
+  //   return this._active;
+  // },
+  //
+  // makeActive: function (exercise) {
+  //
+  // },
+
   render: function () {
+
     var content = this.template({ topic: this.model });
     this.$el.html(content);
     this.attachSubviews();
