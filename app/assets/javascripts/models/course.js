53a5b080a14fdb3fd1111ba275d676dae3763f56
mpiercy827 <mpiercy@gmail.com>
2015-08-12 11:21:57 -0700

    course model parses topics

--- app/assets/javascripts/models/course.js
+++ app/assets/javascripts/models/course.js
@@ -2,7 +2,12 @@ CodeBytes.Models.Course = Backbone.Model.extend({
   urlRoot: "/api/courses",
 
   parse: function (data) {
+    if (data.topics) {
+      this.topics().set(data.topics);
+      delete data.topics;
+    }
 
+    return data;
   },
 
   topics: function () {
