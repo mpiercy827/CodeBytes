47d6d152073138559b5237a123516957c457af26
mpiercy827 <mpiercy@gmail.com>
2015-08-13 14:34:30 -0700

    create global interpreter and options

--- app/assets/javascripts/utils/interpreter.js
+++ app/assets/javascripts/utils/interpreter.js
@@ -1,4 +1,9 @@
-//These are the options that are used for the
+//These are the options that are used for the interpreter.
+InterpreterElements = {
+  $editor: $(".editor"),
+  $editorArea: $(".editor-area"),
+  $terminal: $(".terminal"),
+};
 
 InterpreterOptions = {
   input: function (callback) {
@@ -6,23 +11,43 @@ InterpreterOptions = {
   },
 
   output: function (outputData) {
-
+    if (outputData) {
+      CodeBytes.UserResults.output = outputData;
+    }
   },
 
   result: function (resultData) {
+    CodeBytes.UserResults.result = resultData;
+    var $consoleText = $("<pre>").addClass("output");
 
+    $consoleText.append(CodeBytes.UserResults.output);
+    $consoleText.append("> " + resultData);
+
+    InterpreterElements.$terminal.html($consoleText);
   },
 
   error: function (errorData) {
+    CodeBytes.UserResults.error = errorData;
+    var $consoleText = $("<pre>").addClass("output");
 
+    $consoleText.append(CodeBytes.UserResults.output);
+    $consoleText.append("> " + errorData);
+
+    InterpreterElements.$terminal.html($consoleText);
   },
 
   timeout: {
     time: 10000,
     callback: function () {
-
+      console.log("timed out");
+      CodeBytes.Interpreter.loadLanguage("ruby", function() {
+        console.log("ruby reloaded");
+      });
+      return true;
     }
   }
 };
 
-// CodeBytes.Interpreter = new JSREPL(InterpreterOptions);
+CodeBytes.UserResults = {};
+
+CodeBytes.Interpreter = new JSREPL(InterpreterOptions);
