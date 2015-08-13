8bca95e42811e43ebeff094a44e11af88fe3e7c0
mpiercy827 <mpiercy@gmail.com>
2015-08-13 11:30:28 -0700

    interpreter breaks up errors onto multiple lines

--- public/interpreter.js
+++ public/interpreter.js
@@ -1,18 +1,9 @@
-// var Interpreter = function (options) {
-//     this.language = options.language;
-//     this.loadLanguage();
-// }
-//
-// Interpreter.prototype.loadLanguage = function (first_argument) {
-//   // body...
-// };
-
 var intInput = function (callback) {
   callback();
 }
 
 var intOutput = function (outputData) {
   if (outputData) {
-    
+
   }
 }
