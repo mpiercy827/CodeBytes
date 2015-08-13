f2c1ae4cdda53c9cfac484919bb078a9ee2caf1f
mpiercy827 <mpiercy@gmail.com>
2015-08-13 10:43:34 -0700

    registers change of exercise drop down list

--- app/assets/javascripts/views/topics/topic_show.js
+++ app/assets/javascripts/views/topics/topic_show.js
@@ -2,7 +2,7 @@ CodeBytes.Views.TopicShow = Backbone.CompositeView.extend({
   template: JST["topics/topic_show"],
 
   events: {
-
+    "change .exercise-list": "switchToExercise"
   },
 
   initialize: function () {
@@ -27,18 +27,6 @@ CodeBytes.Views.TopicShow = Backbone.CompositeView.extend({
   switchToExercise: function (exercise) {
     alert("You switched to another exercise!");
   },
-
-  // activeExercise: function () {
-  //   if (!this._active && this.model.exercises().length !== 0) {
-  //     this._active = this.model.exercises().first();
-  //   }
-  //
-  //   return this._active;
-  // },
-  //
-  // makeActive: function (exercise) {
-  //
-  // },
 
   render: function () {
 
